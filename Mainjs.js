e135c39f8dc5e7cc8dac94c1bd9b4d51f5b29afb
1nOCeN <118019311+1nOCeN@users.noreply.github.com>
2024-11-23 00:55:57 +0200

    Add files via upload

--- Mainjs.js
+++ Mainjs.js
@@ -2,3 +2,13 @@ function toggleBox() {
     const box = document.getElementById('box2');
     box.classList.toggle('visible');
 }
+setTimeout(() => {
+    const textElement = document.getElementById("animated-text");
+    const formElement = document.getElementById("login-form");
+
+    // Hide the animated text
+    textElement.style.display = "none";
+
+    // Show the login form
+    formElement.style.display = "flex";
+  }, 3000); // 3 seconds (match the animation duration)
